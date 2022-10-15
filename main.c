dcb89e88f2407eef3473577ecde1dc404695bb7f
0Magenta0 <0_magenta_0@mail.ru>
2022-10-15 18:16:36 +0300

    Upd: Change License Source C Comment Format

--- main.c
+++ main.c
@@ -1,3 +1,10 @@
+/*
+ *  main.c
+ *  This file is a part of the TMCScan distribution released under GNU GPLv3
+ *  Copyright (C) 2022 _Magenta_
+ *  https://github.com/0Magenta0/TMCScan
+ */
+
 #include <stdio.h>
 
 int main (int argc, char **argv) {
