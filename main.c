f468c14369e62dbb9d55f532090f234bc4bf10d6
0Magenta0 <0_magenta_0@mail.ru>
2022-12-23 15:25:39 +0300

    Reformat: C code restyle

--- main.c
+++ main.c
@@ -7,24 +7,9 @@
 
 #include <stdio.h>
 
-#include "cli/cli.h"
-#include "tui/tui.h"
-
-int main (int argc, char **argv) {
-    if (argc > 1) {
-        if (start_cli (argc, argv)) {
-            return 0; /* CLI Mode is enabled. Doesn't need to continue. */
-        }
-    }
-
-    if (start_tui ()) {
-        show_menu ();
-        end_tui ();
-        return 0;
-    } else {
-        end_tui ();
-        fputs ("Curses not initialized normally!\n", stderr);
-        return 1;
-    }
+int
+main (int argc, char **argv)
+{
+    return 0;
 }
 
