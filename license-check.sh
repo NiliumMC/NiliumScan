552f97ff4c9032f4d26dc513ffb74422c334419a
0Magenta0 <0_magenta_0@mail.ru>
2022-12-23 05:18:21 +0300

    Upd: license-check.sh change brackets type

--- license-check.sh
+++ license-check.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for correct_path in $(find * -type f -name "*.c" -or -name "*.h"); do
+for correct_path in $(find * -type f -name '*.c' -or -name '*.h'); do
     if [ "$correct_path" != "$(awk 'BEGIN{} {if(NR == 2){print substr($0,5)}}' $correct_path)" ]; then
         echo -e "[\x1B[31mFAIL\x1B[0m] $correct_path"
     fi
