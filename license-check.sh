3a836c1be08a8bfa0e0813604eeaa91934a214d6
0Magenta0 <0_magenta_0@mail.ru>
2022-12-27 09:33:49 +0300

    Upd: license-check.sh add more checks

--- license-check.sh
+++ license-check.sh
@@ -6,9 +6,21 @@
 #  https://github.com/TuiMC/TMCScan                                            #
 #--------------==================================================--------------#
 
+# File name shouldn't contains the whitespace or wildcards.
 for correct_path in $(find * -type f -name '*.c' -or -name '*.h'); do
     if [ "$correct_path" != "$(awk 'BEGIN{} {if(NR == 2){print substr($0,5)}}' $correct_path)" ]; then
-        echo -e "[\x1B[31mFAIL\x1B[0m] $correct_path"
+        echo -e "[\x1B[31mFAIL - PATH\x1B[0m] $correct_path"
+        continue
+    fi
+
+    if [ '0Magenta0' != "$(awk 'BEGIN{} {if(NR == 4){print substr($0,25)}}' $correct_path)" ]; then
+        echo -e "[\x1B[31mFAIL - NICK\x1B[0m] $correct_path"
+        continue
+    fi
+
+    if [ 'https://github.com/TuiMC/TMCScan' != "$(awk 'BEGIN{} {if(NR == 5){print substr($0,5)}}' $correct_path)" ]; then
+        echo -e "[\x1B[31mFAIL - LINK\x1B[0m] $correct_path"
+        continue
     fi
 done
 
